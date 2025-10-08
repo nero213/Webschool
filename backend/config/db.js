b93540cc5bf3c09707081298caa9f9af5b0dd05f
Robe Kyle P. Salangad <robekyle3@gmail.com>
2025-10-08 17:35:39 +0800

    fix the db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,17 +5,10 @@ dotenv.config();
 
 const pool = mysql
   .createPool({
-<<<<<<< HEAD
     host: process.env.MYSQL_HOST || "localhost",
     user: process.env.MYSQL_USER || "root",
     password: process.env.MYSQL_PASSWORD || "",
     database: process.env.MYSQL_DATABASE || "webschool",
-=======
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
->>>>>>> ff0b0f9 (added delete to complete the crud)
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
