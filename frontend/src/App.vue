8b388542520b23ead225a6cce98554ea65b4ecfc
nero213 <robekyle3@gmail.com>
2025-10-14 15:29:48 +0800

    remove merge conflict

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,7 @@
 <script setup>
 import navbar_app from './components/navbar/navbar_app.vue'
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-import SeeLessons from './components/SeeLessons.vue'
->>>>>>> 5327e46 (added axios to my frontend to fetch data)
-=======
-import SeeLessons from './components/SeeLessons.vue';
-
->>>>>>> fc67376 (adjust the colors to my frontend)
 </script>
 
 <template>
   <navbar_app />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  <SeeLessons />
->>>>>>> 5327e46 (added axios to my frontend to fetch data)
-=======
-  <!-- <SeeLessons /> -->
-
->>>>>>> fc67376 (adjust the colors to my frontend)
 </template>
