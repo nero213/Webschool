41f8951b16a8c483a5eb14974835b2b031b08e6d
nero213 <robekyle3@gmail.com>
2025-10-14 15:13:12 +0800

    got the latest herosection from testing

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://192.168.1.83:3000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
-    
 })
 
-export default http;
+export default http
